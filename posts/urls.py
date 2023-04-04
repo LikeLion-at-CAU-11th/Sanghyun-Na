89365827b5bf203360d527d52ed2da90ee6ea257
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-04-04 19:02:18 +0900

    Challenge

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,7 @@ from posts.views import *
 
 urlpatterns = [
     path('', hello_world, name='hello_world'),
+    path('post_detail', get_all_posts, name='get_all_posts'),
     path('post_detail/<int:id>', get_post_detail, name='get_post_detail'),
 ]
 # <int:pk> 는 int 값을 받고 그걸 pk에 넘긴다는 뜻
--- posts/views.py
+++ posts/views.py
@@ -29,4 +29,30 @@ def get_post_detail(resquest, id):
         'message' : '게시글 조회 성공',
         'data' : category_json
     })
+
+def get_all_posts(resquest):
+    found = True
+    data = []
+    id=1
+    while True:
+        try:
+            post = get_object_or_404(Post, pk = id)
+            category_json ={
+                "id" : id,
+                "writer" : post.writer,
+                "content" : post.content,
+                "category" : post.category,
+            }
+            data.append(category_json)
+            id+=1
+        except:
+            found = False
+        if found == False:
+            break
+
+    return JsonResponse({
+        'status' : 200,
+        'message' : '모든 게시글 조회 성공',
+        'data' : data
+    })
 #전부 불러오는 함수 만들어야 하니 다 반복문으로 전부 json request하고 try except로 404 뜰때 멈추면 될듯?
